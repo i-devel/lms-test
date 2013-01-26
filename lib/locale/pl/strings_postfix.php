41283f4214f573fdfdcb1b5dd72877a5bc1c4aaf
Sylwester Zdanowski <sylwesterzdanowski@gmail.com>
2013-01-26 18:08:25 +0100

    tłumaczenia do postfixa

---
+++ lib/locale/pl/strings_postfix.php
@@ -0,0 +1,26 @@
+<?php
+#strings_postvix v0.3 by Sylwester Zdanowski
+$_LANG['Users list'] = 'Lista userów';
+$_LANG['Postfix users list'] = 'Lista użytkowników postfix';
+$_LANG['Add user'] = 'Nowy user';
+$_LANG['Add new postfix user'] = 'Dodaj nowego użytkownika postfix';
+$_LANG['Domain list'] = 'Lista domen';
+$_LANG['Add domain'] = 'Nowa domena';
+$_LANG['Add Postfix domain'] = 'Dodaj domenę postfix';
+$_LANG['Forwarding list'] = 'Lista przekierowań';
+$_LANG['Postfix forwarding list'] = 'Lista przekierowań postfix';
+$_LANG['Add forwarding'] = 'Nowe przekierowanie';
+$_LANG['Add postfix forwarding'] = 'Dodaj przekierowanie postfix';
+$_LANG['User List, host: $0'] = 'Lista użytkowników, host: $0';
+$_LANG['Add Postfix Domain, host: $0'] = 'Nowa domena postfix, host: $0';
+$_LANG['Add Postfix User, host: $0'] = 'Nowy użytkownik postfix, host: $0';
+$_LANG['Postfix Domain List, host: $0'] = 'Lista domen postfix, host: $0';
+$_LANG['Postfix Forwarding List, host $0'] = 'Lista przekierowań postfix, host: $0';
+$_LANG['Add Postfix Forwarding, host: $0'] = 'Dodaj przekierowanie postfix, host: $0';
+
+$_LANG['Enter e-mail address'] = 'Wprowadź adres e-mail';
+$_LANG['Enter domain'] = 'Wprowadź domenę';
+$_LANG['Domain exists!'] = 'Domena istnieje!';
+$_LANG['Enter source'] = 'Wprowadź źródło';
+$_LANG['Enter destination'] = 'Wprowadź cel';
+?>
